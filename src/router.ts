084263f5c00720b11fc80d2525e42fb06df306c5
Yuurin <byyuurin@gmail.com>
2022-04-13 22:15:26 +0800

    transform routes

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import routes from 'virtual:generated-pages'
+import recordRaws from 'virtual:generated-pages'
+
+function createRoutes() {
+  return recordRaws.sort((a, b) => {
+    const priority = (path: string) => /\(.*\)\*?$/.test(path)
+      ? path.length - Number.MAX_SAFE_INTEGER
+      : path.length
+    return priority(b.path) - priority(a.path)
+  }).map((raw) => {
+    const route = { ...raw }
+    const targetRE = /\/:\w+\(.*\)\*?$/
+
+    if (targetRE.test(route.path))
+      route.path = route.path.replace(targetRE, '/*')
+
+    return route
+  })
+}
+
+export function useRouter() {
+  const instance = getCurrentInstance()!
+  return instance.proxy.$router
+}
+
+export function useRoute() {
+  const instance = getCurrentInstance()!
+  return computed(() => instance.proxy.$route)
+}
 
 Vue.use(VueRouter)
 
+const routes = createRoutes()
 const router = new VueRouter({
   mode: 'history',
   base: import.meta.env.BASE_URL,
   routes,
 })
-
-export const useRouter = () => {
-  const instance = getCurrentInstance()!
-  return instance.proxy.$router
-}
-
-export const useRoute = () => {
-  const instance = getCurrentInstance()!
-  return computed(() => instance.proxy.$route)
-}
 
 export default router
